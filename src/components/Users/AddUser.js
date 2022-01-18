a476a2f5ff3b5a2e5c7292974f2d29d0572ff659
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2022-01-18 18:09:07 -0300

    94. Adding Validation & Resetting Logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,36 +1,57 @@
 import React, { useState } from "react";
 
 import Card from "../UI/Card";
-import classes from './AddUser.module.css'
+import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('')
-  const [enteredAge, setEnteredAge] = useState('')
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log(enteredUsername, enteredAge)
+
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      return
+    }
+
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value)
-  }
+    setEnteredUsername(event.target.value);
+  };
 
   const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value)
-  }
+    setEnteredAge(event.target.value);
+  };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">UserName</label>
-        <input type="text" id="username" value={enteredUsername} onChange={usernameChangeHandler} />
+        <input
+          type="text"
+          id="username"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
 
         <label htmlFor="age">Age (years)</label>
-        <input type="number" id="age" value={enteredAge} onChange={ageChangeHandler} />
+        <input
+          type="number"
+          id="age"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
 
-        <Button type='submit'>Add User</Button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
